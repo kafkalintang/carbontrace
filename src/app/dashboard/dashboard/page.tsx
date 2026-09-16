33cd51db7d6ed2bd983672d9fdcb7dfc5573b5db
kafkalintang <kafkaalintang83@gmail.com>
2026-09-16 10:43:48 +0700

    perubahan dashboard

--- src/app/dashboard/dashboard/page.tsx
+++ src/app/dashboard/dashboard/page.tsx
@@ -15,7 +15,9 @@ export default function DashboardPage() {
             dashboard
           </h1>
 
-          
+          <p className="mt-2 text-slate-600">
+            monitor your carbon footprint and sustainability progress.
+          </p>
         </div>
 
         <DashboardStats />
